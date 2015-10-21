57d6b39709728edf1501e1f5fb05d53d9708c288
BalakinVladislav <balakinv95@gmail.com>
2015-10-21 10:18:14 +0500

    ready

--- phoneBook.js
+++ phoneBook.js
@@ -9,14 +9,14 @@ var phoneBook = []; // Здесь вы храните записи как хот
 module.exports.add = function add(name, phone, email) {
 
     var regExpPhoneNumber = /^((\+\d{1,2}|\d{1,2})[\-]?)?\s?(\(\d{3}\)|\d{3})\s?\d{3}([\-]\d[\-]|\s\d\s|\d)\d{3}$/;
-    var regExpEmailAddress = /^([a-z0-9._-])*@([a-zа-я0-9_-]+\.)+([a-zа-я]+)$/;
+    var regExpEmailAddress = /^([a-z0-9._-])*@([a-zа-я0-9_-]+\.)+([a-zа-я]+)$/i;
 
     if (regExpPhoneNumber.test(phone)&&regExpEmailAddress.test(email)) {
         var contact = {
             name: name,
             phone: phone,
             email: email
-        }
+        };
         phoneBook.push(contact);
         console.log(name + ' был успешно добавлен');
     } 
@@ -36,7 +36,7 @@ module.exports.add = function add(name, phone, email) {
 module.exports.find = function find(query) {
 
     for (var i = 0; i < phoneBook.length; i++) {
-        if (phoneBook[i].name.indexOf(query) > -1 || phoneBook[i].phone.indexOf(query) > -1 || phoneBook[i].email.indexOf(query) > -1) {
+        if (phoneBook[i].name.indexOf(query) > -1 || phoneBook[i].phone.indexOf(query) > -1 || phoneBook[i].email.indexOf(query) > -1 || query === undefined) {
             console.log(phoneBook[i]);
             break;
         }
@@ -62,7 +62,6 @@ module.exports.remove = function remove(query) {
             console.log(phoneBook[i].name + ' удален');
             phoneBook.splice(i,1);
             deletedCount += 1;
-            continue;
         }
     }
     console.log('было удалено ' + deletedCount + ' контактов');
